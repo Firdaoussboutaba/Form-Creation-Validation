3169e18b33cce2d8e925cf6b6903b1a8bc1ce6f7
firdaouss0 <firdaouss.boutaba@gmail.com>
2024-07-15 13:29:29 +0100

    modify files

--- script.js
+++ script.js
@@ -12,25 +12,25 @@ document.addEventListener('DOMContentLoaded', function() {
         let isValid = true;
         let messages = [];
 
-        // Username Validation
+       
         if (username.length < 3) {
             isValid = false;
             messages.push('Username must be at least 3 characters long.');
         }
 
-        // Email Validation
-        if (!email.includes('@') || !email.includes('.')) {
+      
+        if (!email.includes("@") || !email.includes('.')) {
             isValid = false;
             messages.push('Email must contain "@" and "."');
         }
 
-        // Password Validation
+       
         if (password.length < 8) {
             isValid = false;
             messages.push('Password must be at least 8 characters long.');
         }
 
-        // Display feedback
+      
         feedbackDiv.style.display = 'block';
 
         if (isValid) {
